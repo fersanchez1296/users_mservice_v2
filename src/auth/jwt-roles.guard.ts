489dc696ebaf704bafe21f8928055c3afa74a10a
fersanchez1296 <fernandosanchezplacencia@gmail.com>
2025-06-06 15:59:30 -0600

    Se modificaron los authguard

--- src/auth/jwt-roles.guard.ts
+++ src/auth/jwt-roles.guard.ts
@@ -1,45 +1,52 @@
-import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { ROLES_KEY } from '../decorators/roles.decorator';
 
 @Injectable()
 export class RolesGuard extends AuthGuard('jwt') implements CanActivate {
-    constructor(private reflector: Reflector) {
-        super();
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    // Primero verifica el token con AuthGuard
+    const isAuthorized = await super.canActivate(context);
+    if (!isAuthorized) {
+      console.log();
+      return false;
+    }
+    console.log('isAuthorized', isAuthorized);
+
+    // Verifica los roles permitidos para esta ruta
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      // Si no se especifican roles, se permite el acceso
+      return true;
     }
 
-    async canActivate(context: ExecutionContext): Promise<boolean> {
-        // Primero verifica el token con AuthGuard
-        const isAuthorized = await super.canActivate(context);
-        if (!isAuthorized) {
-            console.log()
-            return false;
-        }
-        console.log("isAuthorized", isAuthorized);
+    // Obtén el usuario del contexto
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
 
-        // Verifica los roles permitidos para esta ruta
-        const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
-            context.getHandler(),
-            context.getClass(),
-        ]);
+    // Verifica si el usuario tiene al menos uno de los roles requeridos
+    const hasRole = requiredRoles.some((role) => user.rol?.includes(role));
 
-        if (!requiredRoles) {
-            // Si no se especifican roles, se permite el acceso
-            return true;
-        }
-
-        // Obtén el usuario del contexto
-        const request = context.switchToHttp().getRequest();
-        const user = request.user;
-
-        // Verifica si el usuario tiene al menos uno de los roles requeridos
-        const hasRole = requiredRoles.some(role => user.rol?.includes(role));
-
-        if (!hasRole) {
-            throw new ForbiddenException('No tienes los permisos necesarios para acceder a esta ruta.');
-        }
-
-        return true;
+    if (!hasRole) {
+      throw new ForbiddenException(
+        'No tienes los permisos necesarios para acceder a esta ruta.',
+      );
     }
+
+    return true;
+  }
 }
